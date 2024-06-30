2ce11398ecd11910e2dd45b1768dd7c479874c57
Toxiusus <icek1lla2323@gmail.com>
2024-06-30 18:48:22 +0300

    fix

--- src/components/elements/DatePicker/DatePicker.jsx
+++ src/components/elements/DatePicker/DatePicker.jsx
@@ -1,38 +1,33 @@
-import "./Landing.scss";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { registerLocale, setDefaultLocale } from  "react-datepicker";
-import { ru } from 'date-fns/locale/ru';
-registerLocale('ru', ru)
+import { registerLocale, setDefaultLocale } from "react-datepicker";
+import { ru } from "date-fns/locale/ru";
+registerLocale("ru", ru);
 
 const Landing = () => {
   const [startDate, setStartDate] = useState(new Date("2024/06/08"));
   const [endDate, setEndDate] = useState(new Date("2024/06/10"));
 
   return (
-    <div className="landing">
-      <div className="landing__container container">
-        <div className="datepicker">
-          <DatePicker
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-            selectsStart
-            startDate={startDate}
-            endDate={endDate}
-            locale="ru"
-          />
-          <DatePicker
-            selected={endDate}
-            onChange={(date) => setEndDate(date)}
-            selectsEnd
-            startDate={startDate}
-            endDate={endDate}
-            minDate={startDate}
-            locale="ru"
-          />
-        </div>
-      </div>
+    <div className="datepicker">
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        selectsStart
+        startDate={startDate}
+        endDate={endDate}
+        locale="ru"
+      />
+      <DatePicker
+        selected={endDate}
+        onChange={(date) => setEndDate(date)}
+        selectsEnd
+        startDate={startDate}
+        endDate={endDate}
+        minDate={startDate}
+        locale="ru"
+      />
     </div>
   );
 };
