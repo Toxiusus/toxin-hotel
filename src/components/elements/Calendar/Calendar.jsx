0af6f298f359466d54a7eb654e628da9f80861fc
Toxiusus <icek1lla2323@gmail.com>
2024-07-04 20:23:20 +0300

    fix

--- src/components/elements/Calendar/Calendar.jsx
+++ src/components/elements/Calendar/Calendar.jsx
@@ -1,53 +1,38 @@
-import React from "react";
-import "./Calendar.scss";
+import React, { useState } from "react";
+import './Calendar.scss'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
+import { ru } from "date-fns/locale/ru";
+registerLocale("ru", ru);
 
 const Calendar = () => {
-  const days = [
-    28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-    19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-  ];
-
-  const currentDate = document.querySelector("current-date");
-  //getting new date,current year and month
-  let date = new Date();
-
-  let currYear = date.getFullYear();
-
-  let currMonth = date.getMonth();
-
-  const renderCalendar = () => {
-    currentDate.innerHTML = `${currMonth} ${currYear}`;
-  };
+  const [startDate, setStartDate] = useState(new Date("2024/06/08"));
+  const [endDate, setEndDate] = useState(new Date("2024/06/10"));
 
   return (
-    <div className="wrapper">
-      <header>
-        <p className="current-date">{currentDate}</p>
-        <div className="icons">
-          <span className="material-symbols-rounded">left</span>
-          <span className="material-symbols-rounded">right</span>
-        </div>
-      </header>
-      <div className="calendar">
-        <ul className="weeks list-reset">
-          <li>Пн</li>
-          <li>Вт</li>
-          <li>Ср</li>
-          <li>Чт</li>
-          <li>Пт</li>
-          <li>Сб</li>
-          <li>Вс</li>
-        </ul>
-        <ul className="days list-reset">
-          {days.map((days, i) => (
-            <li className="inactive active" key={i}>
-              {days}
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className="datepicker">
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        selectsStart
+        startDate={startDate}
+        endDate={endDate}
+        locale="ru"
+      />
+      <DatePicker
+        selected={endDate}
+        onChange={(date) => setEndDate(date)}
+        selectsEnd
+        startDate={startDate}
+        endDate={endDate}
+        minDate={startDate}
+        locale="ru"
+      />
     </div>
   );
 };
 
 export default Calendar;
+
+
