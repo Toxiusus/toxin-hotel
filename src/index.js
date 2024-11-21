b01dca4cb73ebbab9b88d5b60bd3a5b20abbc96a
Toxiusus <icek1lla2323@gmail.com>
2024-11-21 14:32:45 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles/main.scss";
+import "./styles/fonts.scss";
+import "./styles/normalize.scss";
+import "./styles/settings.scss";
+import "./styles/header.scss";
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const PugPlugin = require("pug-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+
+module.exports = {
+  plugins: [
+    new PugPlugin({
+      entry: {
+        // define many page templates here
+        index: "src/pages/landing.pug", // => dist/index.html
+      },
+      js: {
+        // JS output filename
+        filename: "js/[name].[contenthash:8].js",
+      },
+      css: {
+        // CSS output filename
+        filename: "css/[name].[contenthash:8].css",
+      },
+    }),
+    new FaviconsWebpackPlugin("./src/assets/favicon.ico"),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(s?css|sass)$/,
+        use: ["css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(ico|png|jp?g|webp|svg)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "img/[name].[hash:8][ext][query]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[hash][ext][query]", // сохраняет изображения в папку fonts
+        },
+      },
+    ],
+  },
+};
