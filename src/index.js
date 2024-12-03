f0604b192432a4ea939f5f242d52abef4feeb1f6
Toxiusus <icek1lla2323@gmail.com>
2024-12-03 15:39:34 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,14 +3,3 @@ import "./styles/fonts.scss";
 import "./styles/normalize.scss";
 import "./styles/settings.scss";
 import "./styles/header.scss";
-import $ from 'jquery';
-import 'jquery-ui/ui/widgets/datepicker'; // Импортируем Datepicker
-import 'jquery-ui/themes/base/theme.css'; // Импортируем базовую тему (при необходимости)
-
-$(document).ready(function () {
-  // Пример использования Datepicker
-  $('#datepicker').datepicker({
-    // Настройки Datepicker
-    dateFormat: 'dd-mm-yy',
-  });
-});
