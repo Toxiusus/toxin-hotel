92b5976cec46461f16568fddaa202643cf50fdef
Toxiusus <icek1lla2323@gmail.com>
2024-12-06 01:07:16 +0300

    fix

--- src/js/datapicker.js
+++ src/js/datapicker.js
@@ -1,13 +1,36 @@
-const datepickerBox = document.querySelector(".datepicker__box"); //datepickerinbox который находится в миксине
-const datepickerInput = document.querySelector(".datepicker__input"); //datepickerinput который находится в миксине
-const datepickerTo = document.querySelector(".datepicker__input-to"); //datepickerinput который находится в миксине
+const datepickerBox = document.querySelector(".datepicker__box");
+const datepickerInput = document.querySelector(".datepicker__input");
+const datepickerTo = document.querySelector(".datepicker__input-to");
+const datepickerDelete = document.querySelector(".datepicker__delete");
+const datepickerSubmit = document.querySelector(".datepicker__submit");
+const datepickerDates = document.querySelector(".datepicker__dates");
+
+let selectedDate = new Date();
+let year = selectedDate.getFullYear();
+let month = selectedDate.getMonth();
 
 datepickerInput.addEventListener("click", () => {
-  //когда нажимаем на инпут тру меняется на фолсе и бок появляется
   datepickerBox.hidden = false;
 });
 
 datepickerTo.addEventListener("click", () => {
-  //когда нажимаем на инпут тру меняется на фолсе и бок появляется
   datepickerBox.hidden = false;
 });
+
+datepickerDelete.addEventListener("click", () => {
+  //когда нажимаем на инпут тру меняется на фолсе и блок появляется
+  datepickerBox.hidden = true;
+});
+
+datepickerSubmit.addEventListener("click", () => {
+  //когда нажимаем на инпут тру меняется на фолсе и блок появляется
+  datepickerBox.hidden = true;
+});
+
+//отображать даты в интерфейсе календаря
+const displayDates = () => {
+  //очистить даты
+  datepickerDates.innerHTML = "";
+};
+
+displayDates();
