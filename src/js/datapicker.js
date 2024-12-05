c00c9081defe75783c222a8ab8f53729da072322
Toxiusus <icek1lla2323@gmail.com>
2024-12-06 00:19:57 +0300

    fix

--- src/js/datapicker.js
+++ src/js/datapicker.js
@@ -1,4 +1,19 @@
-const datepickerBox = document.querySelector("datepicker__box");
-const datepickerInput = document.querySelector("datepicker__input");
+const datepickerBox = document.querySelector(".datepicker__box"); //datepickerinbox который находится в миксине
+const datepickerInput = document.querySelector(".datepicker__input"); //datepickerinput который находится в миксине
+const datepickerTo = document.querySelector(".datepicker__input-to"); //datepickerinput который находится в миксине
+const body = document.querySelector("body");
 
-console.log(datepickerInput);
+datepickerInput.addEventListener("click", () => {
+  //когда нажимаем на инпут тру меняется на фолсе и бок появляется
+  datepickerBox.hidden = false;
+});
+
+datepickerTo.addEventListener("click", () => {
+  //когда нажимаем на инпут тру меняется на фолсе и бок появляется
+  datepickerBox.hidden = false;
+});
+
+body.addEventListener("click", () => {
+  //когда нажимаем на инпут тру меняется на фолсе и бок появляется
+  datepickerBox.hidden = true;
+});
