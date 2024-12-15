c0bfb3ccd227bc02d006e27b768c094592e49e56
Toxiusus <icek1lla2323@gmail.com>
2024-12-16 00:17:25 +0300

    fix

---
+++ src/js/dropdown.js
@@ -0,0 +1,7 @@
+const dropdown = document.querySelector(".dropdown__input")
+const list = document.querySelector(".dropdown__list")
+console.log(dropdown);
+
+dropdown.addEventListener("click",() => {
+    list.toggle
+})
