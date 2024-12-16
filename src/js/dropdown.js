914672558d9b16b262fb297beaa7af738771a0ea
Toxiusus <icek1lla2323@gmail.com>
2024-12-16 13:55:20 +0300

    add javascript for dropdown

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,7 +1,17 @@
-const dropdown = document.querySelector(".dropdown__input")
-const list = document.querySelector(".dropdown__list")
-console.log(dropdown);
+const dropdowns = document.querySelector(".dropdown");
 
-dropdown.addEventListener("click",() => {
-    list.toggle
+const select = document.querySelector('.select')
+
+const caret = document.querySelector('.caret')
+
+const menu = document.querySelector('.menu')
+const options = document.querySelector('.menu li')
+const selected = document.querySelector('.selected')
+
+select.addEventListener('click',() => {
+    select.classList.toggle('select-clicked')
+    caret.classList.toggle('caret-rotate')
 })
+
+
+
